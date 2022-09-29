052c245ebfac5e76bef3f3867050b80671a3b3c1
cheezily <pm655@yahoo.com>
2022-09-29 13:47:41 -0500

    minor ui change

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -10,7 +10,7 @@ import { Link } from '@inertiajs/inertia-vue3';
             </Link>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="bg-gray-200 w-full sm:max-w-md mt-3 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
             <slot />
         </div>
     </div>
