65fc7ddce48199e10a6e7bf7045647d832816ea6
cheezily <pm655@yahoo.com>
2022-10-03 11:35:02 -0500

    remove trim in watchers

--- resources/js/Components/Submit.vue
+++ resources/js/Components/Submit.vue
@@ -1023,7 +1023,6 @@ import axios from 'axios';
 						this.provider_search_results = []
 					}
 				}
-				this.provider_name = this.provider_name.trim()
 			},
 			city(r) {
 				if (!this.prevent_city_search) {
@@ -1033,7 +1032,6 @@ import axios from 'axios';
 						this.city_search_results = []
 					}
 				}
-				this.city = this.city.trim()
 			},
 			type(r) {
 				if(r != 'dedi') {
